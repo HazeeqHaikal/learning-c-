9a8476b1c0afa362671fa5691382e86a7a044e24
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-01 14:03:17 +0800

    finished better calculator

--- better calculator/calculator.cpp
+++ better calculator/calculator.cpp
@@ -6,8 +6,29 @@ int main(){
 
     int num1, num2;
     char choice;
+    int result;
 
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator: ";
+    cin >> choice;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    if(choice == '+'){
+        result = num1 + num2;
+    }else if(choice == '-'){
+        result = num1 - num2;
+    }else if(choice == '/'){
+        result = num1 / num2;
+    }else if(choice == '*'){
+        result = num1 * num2;
+    }else {
+        cout << "Not a valid operator";
+        return 0;
+    }
     
+    cout << num1 << " " << choice << " " << num2 << " = " << result;
 
     return 0;
 }
