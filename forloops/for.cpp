f55efb9bce3f9e9d94354485846b71f6de479461
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-02 19:19:16 +0800

    update for loops

--- forloops/for.cpp
+++ forloops/for.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main(){
 
-    for (int i = 1; i <= 5; i++){
-        cout << i << "\n";
+    int numbers[] = {1, 40, 20, 12, 33};
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++){
+        cout << numbers[i] << "\n";
     }
     
 
