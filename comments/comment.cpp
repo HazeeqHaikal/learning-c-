59819ae51fb1c3b7bce0252a3f0eda2d6f67aed4
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-03 01:41:56 +0800

    comment file

---
+++ comments/comment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // Just some random comment
+    /*
+    Line 1
+    Line 2
+    Line 3
+    Line 4
+    Line 5
+    */
+    // cout << "Comments are fun";
+
+    return 0;
+}
