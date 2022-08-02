7525a64814e5ec121aea57ece44e28bbc63f34f7
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-08-03 00:50:56 +0800

    nested array

---
+++ nest/nest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numberGrid[5][2] = {
+
+        {1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}
+
+    };
+
+    cout << numberGrid[2][1];
+
+    return 0;
+}
